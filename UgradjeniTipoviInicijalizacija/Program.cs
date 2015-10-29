0ae7f4385b53b6399df1b1af6baf40369f3473ca
sasviben <sandro.sviben@hotmail.com>
2015-10-29 21:18:59 +0100

    Inicialization changed

--- UgradjeniTipoviInicijalizacija/Program.cs
+++ UgradjeniTipoviInicijalizacija/Program.cs
@@ -9,6 +9,10 @@ namespace CSharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
+            int a = new int();
+            int b = 4;
+
+            string s1 = "avbs";
         }
     }
 }
